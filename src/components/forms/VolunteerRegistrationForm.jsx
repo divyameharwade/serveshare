5606226e38b71444915685dc633ba71c0a08b08e
Anmol Varshney <varsh@MacBook-Air-191.local>
2024-02-18 04:29:03 -0800

    Removed city, state and zip field

--- src/components/forms/VolunteerRegistrationForm.jsx
+++ src/components/forms/VolunteerRegistrationForm.jsx
@@ -13,9 +13,6 @@ export default function VolunteerRegistration() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [streetAddress, setStreetAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [zipcode, setZipcode] = useState("");
-  const [state, setState] = useState("");
   const [nationalId, setNationalId] = useState("");
   const addressRef = useRef(null);
   const navigate = useNavigate();
@@ -44,9 +41,6 @@ export default function VolunteerRegistration() {
         email,
         password,
         streetAddress,
-        city,
-        zipcode,
-        state,
         nationalId,
         latitude,
         longitude
@@ -72,9 +66,6 @@ export default function VolunteerRegistration() {
     email,
     password,
     streetAddress,
-    city,
-    state,
-    zipcode,
     nationalId,
     lat,
     long
@@ -85,9 +76,6 @@ export default function VolunteerRegistration() {
       email: email,
       password: password,
       streetAddress: streetAddress,
-      city: city,
-      state: state,
-      zipcode: zipcode,
       nationalId: nationalId,
       latitude: lat,
       longitude: long,
@@ -247,62 +235,6 @@ export default function VolunteerRegistration() {
                 </div>
               </div>
 
-              <div className="sm:col-span-2 sm:col-start-1">
-                <label
-                  htmlFor="city"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  City
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    autoComplete="address-level2"
-                    onChange={(e) => setCity(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="region"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  State / Province
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="region"
-                    id="region"
-                    autoComplete="address-level1"
-                    onChange={(e) => setState(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="postal-code"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  ZIP / Postal code
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="postal-code"
-                    id="postal-code"
-                    autoComplete="postal-code"
-                    onChange={(e) => setZipcode(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
               <div className="sm:col-span-3">
                 <label
                   htmlFor="id"
