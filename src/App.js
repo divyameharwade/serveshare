a62affe4c11d6423c3740765be49e8fb782a7439
Anmol Varshney <varsh@MacBook-Air-191.local>
2024-02-17 23:34:36 -0800

    Added Form to add Opportunities

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cards from "./components/Cards";
 import Feed from "./components/Feed";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Filter from "./components/forms/filter";
-
+import AddOpportunity from "./components/forms/AddOpportunity";
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />}/>
+          <Route path="/home" element={<Home />} />
           <Route path="/volunteer" element={<Cards />} />
           <Route path="/feed" element={<Feed />} />
-          <Route path="/filter" element={<Filter />}
-        />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/addOpportunity" element={<AddOpportunity />} />
         </Routes>
       </BrowserRouter>
     </>
